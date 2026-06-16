8f48b3c245dda70de4c3b2a74ab22b962abb0e05
Dean Lark <deanolark@gmail.com>
2026-06-16 18:56:12 +0100

    Set up postgreSQL

--- config/settings.py
+++ config/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "prisdean_db",
+        "USER": "deano_prisdean",
+        "PASSWORD": "prisdean_password",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
