57fbc32169a889a405dee33a62b45961f85ec2b3
Dean Lark <deanolark@gmail.com>
2026-06-18 20:28:49 +0100

    Created the booking form

---
+++ bookings/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import Booking
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ["booking_date", "booking_time"]
+
+         widgets = {
+            "booking_date": forms.DateInput(
+                attrs={
+
+                    "type": "date",
+
+                    "class": "form-control",
+                }
+            ),
+
+            "booking_time": forms.TimeInput(
+
+                attrs={
+                    "type": "time",
+                    "class": "form-control",
+                }
+            ),
+        }
+
+        labels = {
+            "booking_date": "Choose a date",
+            "booking_time": "Choose a time",
+        }
