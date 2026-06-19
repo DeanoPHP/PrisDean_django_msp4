6ab4153a581f4241c71fbd7422d746f52aa0ba31
Dean Lark <deanolark@gmail.com>
2026-06-19 11:45:39 +0100

    Add booking form create_booking template

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .forms import BookingForm
+
+
+@login_required
+def create_booking(request):
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user
+            booking.save()
+
+            messages.success(request, "Your booking has been created.")
+            return redirect("home")
+
+    else:
+        form = BookingForm()
+
+    return render(
+        request,
+        "bookings/create_booking.html",
+        {"form": form},
+    )
