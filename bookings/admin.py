172638047955185dc9e381bd947c5a3fb1b690db
Dean Lark <deanolark@gmail.com>
2026-07-29 19:42:51 +0100

    Customise booking admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
+
 from .models import Booking
 
-# Register your models here.
-admin.site.register(Booking)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "booking_date",
+        "booking_time",
+    )
+
+    list_filter = (
+        "booking_date",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+    )
+
+    ordering = (
+        "booking_date",
+        "booking_time",
+    )
