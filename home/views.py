ac9f78dc8183f9de698c72b107c92c9b5662244a
Dean Lark <deanolark@gmail.com>
2026-08-18 20:01:50 +0100

    Temporarily allow homepage responsive preview

--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
+
+
+@xframe_options_exempt
+def home(request):
+    return render(request, "home/index.html")
 
 
 def home(request):
